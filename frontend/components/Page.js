2e651eb1d79a26d17a43237afd8f64590ab720b8
dondevo <daudomar10@gmail.com>
2018-12-13 14:09:51 -0500

    styling

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -24,6 +24,21 @@ const Inner = styled.div`
   padding: 2rem;
 `;
 
+injectGlobal`
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing:inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+`;
 class Page extends Component {
   render() {
     return (
