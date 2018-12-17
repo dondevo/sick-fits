05905ea85dee0cd425807f559ab29f7e1cea6f78
dondevo <daudomar10@gmail.com>
2018-12-16 19:05:16 -0500

    query and datamodel

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+const Query = {
+    async items(parent, args, ctx, info) {
+        items = forwardTo(db)
+
+    }
+};
 
 module.exports = Query;
